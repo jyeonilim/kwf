4cc0942ab700a761d59b6cbd3bfc41e8e07b023a
jyeon <jyeonilim@gmail.com>
2017-11-03 19:46:04 +0900

    main 작업중

--- static/js/common.js
+++ static/js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+    //메인 상단 롤링
+    $('.main-visual').carouFredSel({
+        width: '100%',
+        visible: 1,
+        play: true,
+        scroll: {
+            pauseOnHover: true,
+            easing: 'swing'
+        }
+    });
+
 });
