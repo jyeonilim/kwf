de57031cf209600eacbe28c98b5139efb5620592
jyeon <jyeonilim@gmail.com>
2017-11-06 18:20:13 +0900

    main 작업중

--- static/js/common.js
+++ static/js/common.js
@@ -11,4 +11,15 @@ $(document).ready(function() {
         }
     });
 
+    //메인 상단 롤링
+    $('.main-department').carouFredSel({
+        width: '100%',
+        visible: 1,
+        play: true,
+        scroll: {
+            pauseOnHover: true,
+            easing: 'swing'
+        }
+    });
+
 });
