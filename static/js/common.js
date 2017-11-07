2bbf109e3772fc6941e0af63a005b3cfc7422ac1
jyeon <jyeonilim@gmail.com>
2017-11-08 01:04:35 +0900

    회원현황 작업중

--- static/js/common.js
+++ static/js/common.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
 
-    //메인 상단 롤링
+    // 메인 상단 롤링
     $('.main-visual').slick({
         arrows: false,
         dots: true
     });
-
+    // 메인 하단 유관부서 롤링
     $('.main-department').slick({
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1
+    });
+
+    $('.tab-fishing li').on('click', function() {
+        var index = $(this).index() + 1;
+        $(this).siblings('li').removeClass('seleted');
+        $(this).addClass('seleted');
+        $('.tab-content').hide();
+        $('.fishing' + index).show();
+        return false;
     });
 
 });
